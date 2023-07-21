97cbf96ef3cfd6dda2f1762f2ebf228a25a49fa7
MIKIO ODA <oda.mikio@gmail.com>
2023-07-21 18:13:39 +0900

    ...

--- btree.ts
+++ btree.ts
@@ -136,7 +136,7 @@ const between = (value: number, start: number, end: number): boolean => {
 	return ((start < value) && (value < end));
 }
 
-const find = (current_node: TreeNode, find_key: number): [TreeNode, Entry] => {
+const find = (upper_node:TreeNode,current_node: TreeNode, find_key: number): [TreeNode,TreeNode, Entry] => {
 	const size = current_node.data.length;
 	for (let index: number = 0; index < size; index++) {
 		const lesser_entry: Entry = current_node.data[index];
@@ -146,65 +146,86 @@ const find = (current_node: TreeNode, find_key: number): [TreeNode, Entry] => {
 			const grater_entry: Entry = current_node.data[index + 1];
 
 			if (find_key === grater_entry.key) {
-				return [current_node, grater_entry];
+				return [upper_node,current_node, grater_entry];
 			}
 
 			if (between(find_key, lesser_entry.key, grater_entry.key)) {
 				if (grater_entry.lesser) {
-					return find(grater_entry.lesser, find_key);
+					return find(current_node,grater_entry.lesser, find_key);
 				}
 			}
 
 			if (find_key > grater_entry.key) {
 				if (grater_entry.grater) {
-					return find(grater_entry.grater, find_key);
+					return find(current_node,grater_entry.grater, find_key);
 				}
 			}
 		}
 
 		// 先頭、中間、終端
 		if (find_key === lesser_entry.key) {
-			return [current_node, lesser_entry];
+			return [upper_node,current_node, lesser_entry];
 		}
 
 		if (find_key < lesser_entry.key) {
 			if (lesser_entry.lesser) {
-				return find(lesser_entry.lesser, find_key);
+				return find(current_node,lesser_entry.lesser, find_key);
 			}
 		}
 
 		if (find_key > lesser_entry.key) {
 			if (lesser_entry.grater) {
-				return find(lesser_entry.grater, find_key);
+				return find(current_node,lesser_entry.grater, find_key);
 			}
 		}
 
 	}
-	return [current_node, null];
+	return [upper_node,current_node, null];
 }
 
+console.log("360 :", find(null,r, 360)[2].value);
 
-let [node, entry] = find(r, 350)
+let [node, entry] = find(null,r, 151)
 
 console.log("003 :", node, entry);
 
-console.log("003 :", find(r, 3)[1].value);
-console.log("100 :", find(r, 100)[1].value);
-console.log("120 :", find(r, 120)[1].value);
-console.log("150 :", find(r, 150)[1].value);
-console.log("200 :", find(r, 200)[1].value);
-console.log("320 :", find(r, 320)[1].value);
-console.log("330 :", find(r, 330)[1].value);
-console.log("340 :", find(r, 340)[1].value);
-console.log("350 :", find(r, 350)[1].value);
-console.log("360 :", find(r, 360)[1].value);
-console.log("400 :", find(r, 400)[1].value);
-console.log("500 :", find(r, 500)[1].value);
-console.log("600 :", find(r, 600)[1].value);
+console.log("003 :", find(null,r, 3)[2].value);
+console.log("100 :", find(null,r, 100)[2].value);
+console.log("120 :", find(null,r, 120)[2].value);
+console.log("150 :", find(null,r, 150)[2].value);
+console.log("200 :", find(null,r, 200)[2].value);
+console.log("320 :", find(null,r, 320)[2].value);
+console.log("330 :", find(null,r, 330)[2].value);
+console.log("340 :", find(null,r, 340)[2].value);
+console.log("350 :", find(null,r, 350)[2].value);
+console.log("360 :", find(null,r, 360)[2].value);
+console.log("400 :", find(null,r, 400)[2].value);
+console.log("500 :", find(null,r, 500)[2].value);
+console.log("600 :", find(null,r, 600)[2].value);
 
 //for (let i = 0; i < 601; i+= 1) {
 //	console.log( i + " :", find(r, i).value);
 //}
 
 
+const insert = (current_node: TreeNode,key:number,value:number):TreeNode => {
+	const [upper, node, entry] = find(null,current_node, key);
+	if (!entry) {
+		for (let index = 0;index < node.data.length; index++) {
+			if (node.data[index].key > key) {
+				node.data.splice(index, 0, {
+					key: key,
+					value: value,
+					lesser: null,
+					grater: null
+				});
+				break;
+			}
+		}
+		return node;
+	}
+}
 
+const result = insert(r, 361, 100);
+
+console.log(result);
