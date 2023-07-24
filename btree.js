cb79d78daaa330431d360e3d3a1578b2dc41c33d
MIKIO ODA <oda.mikio@gmail.com>
2023-07-24 15:18:22 +0900

    ...

--- btree.js
+++ btree.js
@@ -31,119 +31,98 @@ class BTreeNode {
 
 }
 */
-var TreeNode = /** @class */ (function () {
-    function TreeNode() {
-    }
-    return TreeNode;
-}());
 var Entry = /** @class */ (function () {
     function Entry() {
     }
     return Entry;
 }());
 var node_size = 4;
-var r = {
-    data: [
-        {
-            key: 150,
-            value: 3,
-            lesser: {
-                data: [
-                    {
-                        key: 3,
-                        value: 0,
-                        lesser: null,
-                        grater: null
-                    },
-                    {
-                        key: 100,
-                        value: 1,
-                        lesser: null,
-                        grater: null
-                    },
-                    {
-                        key: 120,
-                        value: 2,
-                        lesser: null,
-                        grater: null
-                    }
+var key = 0;
+var value = 1;
+var lesser = 2;
+var grater = 3;
+var d = [
+    [150, 3, 1, null],
+    [200, 4, null, 2],
+    [null, null, null, null],
+    [null, null, null, null],
+    [3, 0, null, null],
+    [100, 1, null, null],
+    [120, 2, null, null],
+    [null, null, null, null],
+    [360, 9, 3, 4],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [320, 5, null, null],
+    [330, 6, null, null],
+    [340, 7, null, null],
+    [350, 8, null, null],
+    [400, 10, null, null],
+    [500, 11, null, null],
+    [600, 12, null, null],
+    [null, null, null, null]
+];
+var field = function (record_no) {
+    var record = d[record_no * 4];
+    return {
+        key: record[key],
+        value: record[value],
+        lesser: record[lesser],
+        grater: record[grater]
+    };
+};
+var r = [
+    {
+        key: 150,
+        value: 3,
+        lesser: [
+            { key: 3, value: 0, lesser: null, grater: null },
+            { key: 100, value: 1, lesser: null, grater: null },
+            { key: 120, value: 2, lesser: null, grater: null },
+            { key: null, value: null, lesser: null, grater: null },
+        ],
+        grater: null
+    },
+    {
+        key: 200,
+        value: 4,
+        lesser: null,
+        grater: [
+            {
+                key: 360,
+                value: 9,
+                lesser: [
+                    { key: 320, value: 5, lesser: null, grater: null },
+                    { key: 330, value: 6, lesser: null, grater: null },
+                    { key: 340, value: 7, lesser: null, grater: null },
+                    { key: 350, value: 8, lesser: null, grater: null }
+                ],
+                grater: [
+                    { key: 400, value: 10, lesser: null, grater: null },
+                    { key: 500, value: 11, lesser: null, grater: null },
+                    { key: 600, value: 12, lesser: null, grater: null },
+                    { key: null, value: null, lesser: null, grater: null }
                 ]
             },
-            grater: null
-        },
-        {
-            key: 200,
-            value: 4,
-            lesser: null,
-            grater: {
-                data: [
-                    {
-                        key: 360,
-                        value: 9,
-                        lesser: {
-                            data: [
-                                {
-                                    key: 320,
-                                    value: 5,
-                                    lesser: null,
-                                    grater: null
-                                },
-                                {
-                                    key: 330,
-                                    value: 6,
-                                    lesser: null,
-                                    grater: null
-                                }, {
-                                    key: 340,
-                                    value: 7,
-                                    lesser: null,
-                                    grater: null
-                                }, {
-                                    key: 350,
-                                    value: 8,
-                                    lesser: null,
-                                    grater: null
-                                }
-                            ]
-                        },
-                        grater: {
-                            data: [
-                                {
-                                    key: 400,
-                                    value: 10,
-                                    lesser: null,
-                                    grater: null
-                                },
-                                {
-                                    key: 500,
-                                    value: 11,
-                                    lesser: null,
-                                    grater: null
-                                },
-                                {
-                                    key: 600,
-                                    value: 12,
-                                    lesser: null,
-                                    grater: null
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-    ]
-};
+            { key: null, value: null, lesser: null, grater: null },
+            { key: null, value: null, lesser: null, grater: null },
+            { key: null, value: null, lesser: null, grater: null }
+        ]
+    },
+    { key: null, value: null, lesser: null, grater: null },
+    { key: null, value: null, lesser: null, grater: null }
+];
 var between = function (value, start, end) {
     return ((start < value) && (value < end));
 };
 var find = function (upper_node, current_node, find_key) {
-    var size = current_node.data.length;
-    for (var index = 0; index < size; index++) {
-        var lesser_entry = current_node.data[index];
+    var _size = size(current_node); // current_node.length;
+    for (var index = 0; index < _size; index++) {
+        var lesser_entry = current_node[index];
         // 先頭、中間
-        if (index < size - 1) {
-            var grater_entry = current_node.data[index + 1];
+        if (index < _size - 1) {
+            var grater_entry = current_node[index + 1];
             if (find_key === grater_entry.key) {
                 return [upper_node, current_node, grater_entry];
             }
@@ -175,14 +154,25 @@ var find = function (upper_node, current_node, find_key) {
     }
     return [upper_node, current_node, null];
 };
+var size = function (current_node) {
+    var result = node_size;
+    for (var index = 0; index < node_size; index++) {
+        var entry = current_node[index];
+        if (entry.key == null) {
+            result = index;
+            break;
+        }
+    }
+    return result;
+};
 var is_full = function (current_node) {
-    return (node_size === current_node.data.length);
+    return (node_size === size(current_node));
 };
 var compare = function (current_node, key) {
     var result = 0;
-    var size = current_node.data.length;
-    var min = current_node.data[0];
-    var max = current_node.data[size - 1];
+    var _size = size(current_node);
+    var min = current_node[0];
+    var max = current_node[_size - 1];
     if (key < min.key) {
         result = -1;
     }
@@ -192,68 +182,73 @@ var compare = function (current_node, key) {
     return result;
 };
 var insert_entry = function (current_node, key, value) {
-    var size = current_node.data.length;
-    for (var index = 0; index < size; index++) {
-        if (current_node.data[index].key > key) {
-            current_node.data.splice(index, 0, {
+    var _size = size(current_node);
+    for (var index = 0; index < _size; index++) {
+        if (current_node[index].key > key) {
+            current_node[index] = {
                 key: key,
                 value: value,
                 lesser: null,
                 grater: null
-            });
+            };
             break;
         }
-        else if (index === size - 1) {
-            current_node.data.push({
+        else if (index === _size - 1) {
+            current_node[_size] = {
                 key: key,
                 value: value,
                 lesser: null,
-                grater: current_node.data[index].grater
-            });
-            current_node.data[index].grater = null;
+                grater: current_node[index].grater
+            };
+            current_node[index].grater = null;
         }
     }
+    return current_node;
+};
+var split = function (current_node, key, value) {
     return current_node;
 };
 var insert = function (current_node, key, value) {
     var result = null;
     var _a = find(null, current_node, key), upper_target = _a[0], target = _a[1], entry = _a[2];
-    if (compare(target, key) === -1) { // 子のminより小さい
-        if (!is_full(upper_target)) { // 親に空きがある
-            if (!entry) { // target_nodeに同じキーはない
+    if (!entry) { // target_nodeに同じキーはない
+        if (compare(target, key) === -1) { // 子のminより小さい
+            if (!is_full(upper_target)) { // 親に空きがある
                 result = insert_entry(upper_target, key, value);
             }
+            else {
+                result = split(upper_target, key, value);
+            }
         }
-    }
-    else {
-        if (!is_full(target)) { // 空きがある
-            result = insert_entry(target, key, value);
+        else {
+            if (!is_full(target)) { // 空きがある
+                result = insert_entry(target, key, value);
+            }
+            else {
+                result = split(target, key, value);
+            }
         }
     }
     return result;
 };
 var result = insert(r, 361, 100);
-console.log(result);
-console.log(r);
-/*
-console.log("360 :", find(null,r, 360)[2].value);
-
-let [node, entry] = find(null,r, 151)
-
-console.log("003 :", node, entry);
-
-console.log("003 :", find(null,r, 3)[2].value);
-console.log("100 :", find(null,r, 100)[2].value);
-console.log("120 :", find(null,r, 120)[2].value);
-console.log("150 :", find(null,r, 150)[2].value);
-console.log("200 :", find(null,r, 200)[2].value);
-console.log("320 :", find(null,r, 320)[2].value);
-console.log("330 :", find(null,r, 330)[2].value);
-console.log("340 :", find(null,r, 340)[2].value);
-console.log("350 :", find(null,r, 350)[2].value);
-console.log("360 :", find(null,r, 360)[2].value);
-console.log("400 :", find(null,r, 400)[2].value);
-console.log("500 :", find(null,r, 500)[2].value);
-console.log("600 :", find(null,r, 600)[2].value);
-*/
+// console.log(result);
+//console.log(r);
+//console.log("360 :", find(null, r, 360)[2].value);
+//let [node, entry] = find(null, r, 151)
+//console.log("003 :", node, entry);
+console.log("003 :", find(null, r, 3)[2].value);
+console.log("100 :", find(null, r, 100)[2].value);
+console.log("120 :", find(null, r, 120)[2].value);
+console.log("150 :", find(null, r, 150)[2].value);
+console.log("200 :", find(null, r, 200)[2].value);
+console.log("320 :", find(null, r, 320)[2].value);
+console.log("330 :", find(null, r, 330)[2].value);
+console.log("340 :", find(null, r, 340)[2].value);
+console.log("350 :", find(null, r, 350)[2].value);
+console.log("360 :", find(null, r, 360)[2].value);
+console.log("361 :", find(null, r, 361)[2].value);
+console.log("400 :", find(null, r, 400)[2].value);
+console.log("500 :", find(null, r, 500)[2].value);
+console.log("600 :", find(null, r, 600)[2].value);
 //# sourceMappingURL=btree.js.map
--- btree.ts
+++ btree.ts
@@ -32,22 +32,11 @@ class BTreeNode {
 }
 */
 
-class TreeNode {
-	public data: Entry[];
-}
-
 class Entry {
 	public key: number;
 	public value: number;
-	public lesser: TreeNode;
-	public grater: TreeNode;
-}
-
-class _Entry {
-	public key: number;
-	public value: number;
-	public lesser: number;
-	public grater: number;
+	public lesser: Entry[];
+	public grater: Entry[];
 }
 
 const node_size = 4;
@@ -80,7 +69,7 @@ let d: any[][] = [
 	[null, null, null, null]
 ];
 
-const field = (record_no: number): _Entry => {
+const field = (record_no: number): Entry => {
 	const record = d[record_no * 4];
 	return {
 		key: record[key],
@@ -91,106 +80,61 @@ const field = (record_no: number): _Entry => {
 
 }
 
-let r: TreeNode = {
-	data: [
+let r: Entry[] =
+	[
 		{
 			key: 150,
 			value: 3,
-			lesser: {
-				data: [
-					{
-						key: 3,
-						value: 0,
-						lesser: null,
-						grater: null
-					},
-					{
-						key: 100,
-						value: 1,
-						lesser: null,
-						grater: null
-					},
-					{
-						key: 120,
-						value: 2,
-						lesser: null,
-						grater: null
-					}],
-			},
+			lesser: [
+				{key: 3, value: 0, lesser: null, grater: null},
+				{key: 100, value: 1, lesser: null, grater: null},
+				{key: 120, value: 2, lesser: null, grater: null},
+				{key: null, value: null, lesser: null, grater: null},
+			],
 			grater: null
 		},
 		{
 			key: 200,
 			value: 4,
 			lesser: null,
-			grater: {
-				data: [
-					{
-						key: 360,
-						value: 9,
-						lesser: {
-							data: [
-								{
-									key: 320,
-									value: 5,
-									lesser: null,
-									grater: null
-								},
-								{
-									key: 330,
-									value: 6,
-									lesser: null,
-									grater: null
-								}, {
-									key: 340,
-									value: 7,
-									lesser: null,
-									grater: null
-								}, {
-									key: 350,
-									value: 8,
-									lesser: null,
-									grater: null
-								}],
-						},
-						grater: {
-							data: [
-								{
-									key: 400,
-									value: 10,
-									lesser: null,
-									grater: null
-								},
-								{
-									key: 500,
-									value: 11,
-									lesser: null,
-									grater: null
-								},
-								{
-									key: 600,
-									value: 12,
-									lesser: null,
-									grater: null
-								}],
-						}
-					}],
-			},
-		}],
-}
+			grater: [
+				{
+					key: 360,
+					value: 9,
+					lesser: [
+						{key: 320, value: 5, lesser: null, grater: null},
+						{key: 330, value: 6, lesser: null, grater: null},
+						{key: 340, value: 7, lesser: null, grater: null},
+						{key: 350, value: 8, lesser: null, grater: null}
+					],
+					grater: [
+						{key: 400, value: 10, lesser: null, grater: null},
+						{key: 500, value: 11, lesser: null, grater: null},
+						{key: 600, value: 12, lesser: null, grater: null},
+						{key: null, value: null, lesser: null, grater: null}
+					],
+				},
+				{key: null, value: null, lesser: null, grater: null},
+				{key: null, value: null, lesser: null, grater: null},
+				{key: null, value: null, lesser: null, grater: null}],
+		},
+		{key: null, value: null, lesser: null, grater: null},
+		{key: null, value: null, lesser: null, grater: null}
+	]
 
 const between = (value: number, start: number, end: number): boolean => {
 	return ((start < value) && (value < end));
 }
 
-const find = (upper_node: TreeNode, current_node: TreeNode, find_key: number): [TreeNode, TreeNode, Entry] => {
-	const size = current_node.data.length;
-	for (let index: number = 0; index < size; index++) {
-		const lesser_entry: Entry = current_node.data[index];
+
+const find = (upper_node: Entry[], current_node: Entry[], find_key: number): [Entry[], Entry[], Entry] => {
+	const _size = size(current_node);  // current_node.length;
+	for (let index: number = 0; index < _size; index++) {
+		const lesser_entry: Entry = current_node[index];
 
 		// 先頭、中間
-		if (index < size - 1) {
-			const grater_entry: Entry = current_node.data[index + 1];
+		if (index < _size - 1) {
+			const grater_entry: Entry = current_node[index + 1];
 
 			if (find_key === grater_entry.key) {
 				return [upper_node, current_node, grater_entry];
@@ -230,15 +174,27 @@ const find = (upper_node: TreeNode, current_node: TreeNode, find_key: number): [
 	return [upper_node, current_node, null];
 }
 
-const is_full = (current_node: TreeNode): boolean => {
-	return (node_size === current_node.data.length);
+const size = (current_node: Entry[]): number => {
+	let result = node_size;
+	for (let index = 0; index < node_size; index++) {
+		const entry: Entry = current_node[index];
+		if (entry.key == null) {
+			result = index;
+			break;
+		}
+	}
+	return result;
 }
 
-const compare = (current_node: TreeNode, key: number): number => {
+const is_full = (current_node: Entry[]): boolean => {
+	return (node_size === size(current_node));
+}
+
+const compare = (current_node: Entry[], key: number): number => {
 	let result: number = 0;
-	const size = current_node.data.length;
-	const min: Entry = current_node.data[0];
-	const max: Entry = current_node.data[size - 1];
+	const _size = size(current_node);
+	const min: Entry = current_node[0];
+	const max: Entry = current_node[_size - 1];
 	if (key < min.key) {
 		result = -1;
 	} else if (key > max.key) {
@@ -247,38 +203,37 @@ const compare = (current_node: TreeNode, key: number): number => {
 	return result;
 }
 
-const insert_entry = (current_node: TreeNode, key: number, value: number): TreeNode => {
-
-	const size = current_node.data.length;
-	for (let index = 0; index < size; index++) {
-		if (current_node.data[index].key > key) {
-			current_node.data.splice(index, 0, {
+const insert_entry = (current_node: Entry[], key: number, value: number): Entry[] => {
+	const _size = size(current_node);
+	for (let index = 0; index < _size; index++) {
+		if (current_node[index].key > key) {
+			current_node[index] = {
 				key: key,
 				value: value,
 				lesser: null,
 				grater: null
-			});
+			}
 			break;
-		} else if (index === size - 1) {
-			current_node.data.push({
+		} else if (index === _size - 1) {
+			current_node[_size] = {
 				key: key,
 				value: value,
 				lesser: null,
-				grater: current_node.data[index].grater
-			});
-			current_node.data[index].grater = null;
+				grater: current_node[index].grater
+			}
+			current_node[index].grater = null;
 		}
 	}
 
 	return current_node;
 }
 
-const split = (current_node: TreeNode, key: number, value: number): TreeNode => {
+const split = (current_node: Entry[], key: number, value: number): Entry[] => {
 	return current_node
 }
 
-const insert = (current_node: TreeNode, key: number, value: number): TreeNode => {
-	let result: TreeNode = null;
+const insert = (current_node: Entry[], key: number, value: number): Entry[] => {
+	let result: Entry[] = null;
 	const [upper_target, target, entry] = find(null, current_node, key);
 	if (!entry) { // target_nodeに同じキーはない
 		if (compare(target, key) === -1) { // 子のminより小さい
@@ -300,29 +255,29 @@ const insert = (current_node: TreeNode, key: number, value: number): TreeNode =>
 
 const result = insert(r, 361, 100);
 
-console.log(result);
-console.log(r);
+// console.log(result);
+//console.log(r);
 
 
-/*
-console.log("360 :", find(null,r, 360)[2].value);
+//console.log("360 :", find(null, r, 360)[2].value);
 
-let [node, entry] = find(null,r, 151)
+//let [node, entry] = find(null, r, 151)
 
-console.log("003 :", node, entry);
+//console.log("003 :", node, entry);
 
-console.log("003 :", find(null,r, 3)[2].value);
-console.log("100 :", find(null,r, 100)[2].value);
-console.log("120 :", find(null,r, 120)[2].value);
-console.log("150 :", find(null,r, 150)[2].value);
-console.log("200 :", find(null,r, 200)[2].value);
-console.log("320 :", find(null,r, 320)[2].value);
-console.log("330 :", find(null,r, 330)[2].value);
-console.log("340 :", find(null,r, 340)[2].value);
-console.log("350 :", find(null,r, 350)[2].value);
-console.log("360 :", find(null,r, 360)[2].value);
-console.log("400 :", find(null,r, 400)[2].value);
-console.log("500 :", find(null,r, 500)[2].value);
-console.log("600 :", find(null,r, 600)[2].value);
-*/
+console.log("003 :", find(null, r, 3)[2].value);
+console.log("100 :", find(null, r, 100)[2].value);
+console.log("120 :", find(null, r, 120)[2].value);
+console.log("150 :", find(null, r, 150)[2].value);
+console.log("200 :", find(null, r, 200)[2].value);
+console.log("320 :", find(null, r, 320)[2].value);
+console.log("330 :", find(null, r, 330)[2].value);
+console.log("340 :", find(null, r, 340)[2].value);
+console.log("350 :", find(null, r, 350)[2].value);
+console.log("360 :", find(null, r, 360)[2].value);
+console.log("361 :", find(null, r, 361)[2].value);
+console.log("400 :", find(null, r, 400)[2].value);
+console.log("500 :", find(null, r, 500)[2].value);
+console.log("600 :", find(null, r, 600)[2].value);
+
 
