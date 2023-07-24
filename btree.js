4e4f2359439d6c2ecf4f7ed20eb25425d57d5d7a
MIKIO ODA <oda.mikio@gmail.com>
2023-07-24 13:35:09 +0900

    ...

--- btree.js
+++ btree.js
@@ -31,7 +31,6 @@ class BTreeNode {
 
 }
 */
-//const root = new BTreeNode(true);
 var TreeNode = /** @class */ (function () {
     function TreeNode() {
     }
@@ -42,6 +41,7 @@ var Entry = /** @class */ (function () {
     }
     return Entry;
 }());
+var node_size = 4;
 var r = {
     data: [
         {
@@ -79,12 +79,28 @@ var r = {
                 data: [
                     {
                         key: 360,
-                        value: 6,
+                        value: 9,
                         lesser: {
                             data: [
                                 {
                                     key: 320,
                                     value: 5,
+                                    lesser: null,
+                                    grater: null
+                                },
+                                {
+                                    key: 330,
+                                    value: 6,
+                                    lesser: null,
+                                    grater: null
+                                }, {
+                                    key: 340,
+                                    value: 7,
+                                    lesser: null,
+                                    grater: null
+                                }, {
+                                    key: 350,
+                                    value: 8,
                                     lesser: null,
                                     grater: null
                                 }
@@ -94,19 +110,19 @@ var r = {
                             data: [
                                 {
                                     key: 400,
-                                    value: 7,
+                                    value: 10,
                                     lesser: null,
                                     grater: null
                                 },
                                 {
                                     key: 500,
-                                    value: 8,
+                                    value: 11,
                                     lesser: null,
                                     grater: null
                                 },
                                 {
                                     key: 600,
-                                    value: 9,
+                                    value: 12,
                                     lesser: null,
                                     grater: null
                                 }
@@ -121,7 +137,7 @@ var r = {
 var between = function (value, start, end) {
     return ((start < value) && (value < end));
 };
-var find = function (current_node, find_key) {
+var find = function (upper_node, current_node, find_key) {
     var size = current_node.data.length;
     for (var index = 0; index < size; index++) {
         var lesser_entry = current_node.data[index];
@@ -129,44 +145,115 @@ var find = function (current_node, find_key) {
         if (index < size - 1) {
             var grater_entry = current_node.data[index + 1];
             if (find_key === grater_entry.key) {
-                return grater_entry;
+                return [upper_node, current_node, grater_entry];
             }
             if (between(find_key, lesser_entry.key, grater_entry.key)) {
                 if (grater_entry.lesser) {
-                    return find(grater_entry.lesser, find_key);
+                    return find(current_node, grater_entry.lesser, find_key);
                 }
             }
             if (find_key > grater_entry.key) {
                 if (grater_entry.grater) {
-                    return find(grater_entry.grater, find_key);
+                    return find(current_node, grater_entry.grater, find_key);
                 }
             }
         }
         // 先頭、中間、終端
         if (find_key === lesser_entry.key) {
-            return lesser_entry;
+            return [upper_node, current_node, lesser_entry];
         }
         if (find_key < lesser_entry.key) {
             if (lesser_entry.lesser) {
-                return find(lesser_entry.lesser, find_key);
+                return find(current_node, lesser_entry.lesser, find_key);
             }
         }
         if (find_key > lesser_entry.key) {
             if (lesser_entry.grater) {
-                return find(lesser_entry.grater, find_key);
+                return find(current_node, lesser_entry.grater, find_key);
             }
         }
     }
-    return undefined;
+    return [upper_node, current_node, null];
 };
-console.log("003 :", find(r, 3).value);
-console.log("100 :", find(r, 100).value);
-console.log("120 :", find(r, 120).value);
-console.log("150 :", find(r, 150).value);
-console.log("200 :", find(r, 200).value);
-console.log("320 :", find(r, 320).value);
-console.log("360 :", find(r, 360).value);
-console.log("400 :", find(r, 400).value);
-console.log("500 :", find(r, 500).value);
-console.log("600 :", find(r, 600).value);
+var is_full = function (current_node) {
+    return (node_size === current_node.data.length);
+};
+var compare = function (current_node, key) {
+    var result = 0;
+    var size = current_node.data.length;
+    var min = current_node.data[0];
+    var max = current_node.data[size - 1];
+    if (key < min.key) {
+        result = -1;
+    }
+    else if (key > max.key) {
+        result = 1;
+    }
+    return result;
+};
+var insert_entry = function (current_node, key, value) {
+    var size = current_node.data.length;
+    for (var index = 0; index < size; index++) {
+        if (current_node.data[index].key > key) {
+            current_node.data.splice(index, 0, {
+                key: key,
+                value: value,
+                lesser: null,
+                grater: null
+            });
+            break;
+        }
+        else if (index === size - 1) {
+            current_node.data.push({
+                key: key,
+                value: value,
+                lesser: null,
+                grater: current_node.data[index].grater
+            });
+            current_node.data[index].grater = null;
+        }
+    }
+    return current_node;
+};
+var insert = function (current_node, key, value) {
+    var result = null;
+    var _a = find(null, current_node, key), upper_target = _a[0], target = _a[1], entry = _a[2];
+    if (compare(target, key) === -1) { // 子のminより小さい
+        if (!is_full(upper_target)) { // 親に空きがある
+            if (!entry) { // target_nodeに同じキーはない
+                result = insert_entry(upper_target, key, value);
+            }
+        }
+    }
+    else {
+        if (!is_full(target)) { // 空きがある
+            result = insert_entry(target, key, value);
+        }
+    }
+    return result;
+};
+var result = insert(r, 361, 100);
+console.log(result);
+console.log(r);
+/*
+console.log("360 :", find(null,r, 360)[2].value);
+
+let [node, entry] = find(null,r, 151)
+
+console.log("003 :", node, entry);
+
+console.log("003 :", find(null,r, 3)[2].value);
+console.log("100 :", find(null,r, 100)[2].value);
+console.log("120 :", find(null,r, 120)[2].value);
+console.log("150 :", find(null,r, 150)[2].value);
+console.log("200 :", find(null,r, 200)[2].value);
+console.log("320 :", find(null,r, 320)[2].value);
+console.log("330 :", find(null,r, 330)[2].value);
+console.log("340 :", find(null,r, 340)[2].value);
+console.log("350 :", find(null,r, 350)[2].value);
+console.log("360 :", find(null,r, 360)[2].value);
+console.log("400 :", find(null,r, 400)[2].value);
+console.log("500 :", find(null,r, 500)[2].value);
+console.log("600 :", find(null,r, 600)[2].value);
+*/
 //# sourceMappingURL=btree.js.map
